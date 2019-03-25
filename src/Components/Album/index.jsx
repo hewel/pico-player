1642a387dcd261c7c3b1e37dedc7e76e2782361a
Hewel <hewelzei@qq.com>
2019-03-26 00:26:21 +0800

    refactor(Album): Change Album from Class Component to Function Component

--- src/Components/Album/index.jsx
+++ src/Components/Album/index.jsx
@@ -1,25 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { album, albumImg } from './style.sass'
 import defAlbumImage from 'assets/music-cover.png'
 
-export default class Album extends Component {
+export default function Album(props) {
+    const { className, imageUrl, songName, ...otherProps } = props
+    const [albumImageUrl, albumAlt, albumClassName] = [
+        imageUrl || defAlbumImage,
+        songName || '没有歌曲',
+        className ? `${album} ${className}` : album,
+    ]
 
-    render() {
-        const { className, imageUrl, songName, ...otherProps } = this.props
-        const [albumImageUrl, albumAlt, albumClassName] = [
-            imageUrl || defAlbumImage,
-            songName || '没有歌曲',
-            className ? `${album} ${className}` : album,
-        ]
-
-        return(
-            <div className={albumClassName} {...otherProps}>
-                <img
-                    src={albumImageUrl}
-                    alt={albumAlt}
-                    className={`${albumImg}`}
-                />
-            </div>
-        )
-    }
+    return(
+        <div className={albumClassName} {...otherProps}>
+            <img
+                src={albumImageUrl}
+                alt={albumAlt}
+                className={`${albumImg}`}
+            />
+        </div>
+    )
 }
